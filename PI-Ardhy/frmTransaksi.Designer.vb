819e6f060f2a9549d366709bd1b02ef25fd4d497
andrianoktiawan <andrianoktiawan@gmail.com>
2023-07-18 15:22:45 +0700

    now button tambah can add items to datagridview & when button bayar pressed, textbox shows total and kembalian in IDR Currency format

--- PI-Ardhy/frmTransaksi.Designer.vb
+++ PI-Ardhy/frmTransaksi.Designer.vb
@@ -23,10 +23,6 @@ Partial Class frmTransaksi
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.btnBayar = New System.Windows.Forms.Button()
         Me.btnBack = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -51,46 +47,25 @@ Partial Class frmTransaksi
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 66)
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 61)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(427, 238)
+        Me.DataGridView1.Size = New System.Drawing.Size(427, 221)
         Me.DataGridView1.TabIndex = 0
-        '
-        'Column1
-        '
-        Me.Column1.HeaderText = "Kode Barang"
-        Me.Column1.Name = "Column1"
-        '
-        'Column2
-        '
-        Me.Column2.HeaderText = "Nama Barang"
-        Me.Column2.Name = "Column2"
-        '
-        'Column3
-        '
-        Me.Column3.HeaderText = "Harga"
-        Me.Column3.Name = "Column3"
-        '
-        'Column4
-        '
-        Me.Column4.HeaderText = "Quantity"
-        Me.Column4.Name = "Column4"
         '
         'btnBayar
         '
-        Me.btnBayar.Location = New System.Drawing.Point(474, 281)
+        Me.btnBayar.Location = New System.Drawing.Point(474, 261)
         Me.btnBayar.Name = "btnBayar"
-        Me.btnBayar.Size = New System.Drawing.Size(95, 23)
+        Me.btnBayar.Size = New System.Drawing.Size(95, 21)
         Me.btnBayar.TabIndex = 1
         Me.btnBayar.Text = "Bayar"
         Me.btnBayar.UseVisualStyleBackColor = True
         '
         'btnBack
         '
-        Me.btnBack.Location = New System.Drawing.Point(652, 281)
+        Me.btnBack.Location = New System.Drawing.Point(652, 261)
         Me.btnBack.Name = "btnBack"
-        Me.btnBack.Size = New System.Drawing.Size(75, 23)
+        Me.btnBack.Size = New System.Drawing.Size(75, 21)
         Me.btnBack.TabIndex = 2
         Me.btnBack.Text = "Kembali"
         Me.btnBack.UseVisualStyleBackColor = True
@@ -99,7 +74,7 @@ Partial Class frmTransaksi
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(482, 84)
+        Me.Label1.Location = New System.Drawing.Point(482, 78)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(38, 19)
         Me.Label1.TabIndex = 3
@@ -107,16 +82,16 @@ Partial Class frmTransaksi
         '
         'btnTambah
         '
-        Me.btnTambah.Location = New System.Drawing.Point(571, 281)
+        Me.btnTambah.Location = New System.Drawing.Point(571, 261)
         Me.btnTambah.Name = "btnTambah"
-        Me.btnTambah.Size = New System.Drawing.Size(75, 23)
+        Me.btnTambah.Size = New System.Drawing.Size(75, 21)
         Me.btnTambah.TabIndex = 5
         Me.btnTambah.Text = "Tambah"
         Me.btnTambah.UseVisualStyleBackColor = True
         '
         'txtName
         '
-        Me.txtName.Location = New System.Drawing.Point(673, 12)
+        Me.txtName.Location = New System.Drawing.Point(673, 11)
         Me.txtName.Name = "txtName"
         Me.txtName.ReadOnly = True
         Me.txtName.Size = New System.Drawing.Size(100, 20)
@@ -124,7 +99,7 @@ Partial Class frmTransaksi
         '
         'txtDate
         '
-        Me.txtDate.Location = New System.Drawing.Point(673, 38)
+        Me.txtDate.Location = New System.Drawing.Point(673, 35)
         Me.txtDate.Name = "txtDate"
         Me.txtDate.ReadOnly = True
         Me.txtDate.Size = New System.Drawing.Size(100, 20)
@@ -134,7 +109,7 @@ Partial Class frmTransaksi
         '
         Me.Kasir.AutoSize = True
         Me.Kasir.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Kasir.Location = New System.Drawing.Point(629, 14)
+        Me.Kasir.Location = New System.Drawing.Point(629, 13)
         Me.Kasir.Name = "Kasir"
         Me.Kasir.Size = New System.Drawing.Size(38, 15)
         Me.Kasir.TabIndex = 8
@@ -144,7 +119,7 @@ Partial Class frmTransaksi
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(629, 40)
+        Me.Label2.Location = New System.Drawing.Point(629, 37)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(32, 15)
         Me.Label2.TabIndex = 9
@@ -153,14 +128,14 @@ Partial Class frmTransaksi
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(526, 85)
+        Me.ComboBox1.Location = New System.Drawing.Point(526, 79)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 22)
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 10
         '
         'NumericUpDown1
         '
-        Me.NumericUpDown1.Location = New System.Drawing.Point(526, 157)
+        Me.NumericUpDown1.Location = New System.Drawing.Point(526, 146)
         Me.NumericUpDown1.Name = "NumericUpDown1"
         Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
         Me.NumericUpDown1.TabIndex = 12
@@ -169,7 +144,7 @@ Partial Class frmTransaksi
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Times New Roman", 26.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(35, 393)
+        Me.Label3.Location = New System.Drawing.Point(35, 365)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(92, 41)
         Me.Label3.TabIndex = 13
@@ -178,7 +153,7 @@ Partial Class frmTransaksi
         'TextBox1
         '
         Me.TextBox1.Font = New System.Drawing.Font("Times New Roman", 26.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(133, 390)
+        Me.TextBox1.Location = New System.Drawing.Point(133, 362)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(528, 48)
         Me.TextBox1.TabIndex = 14
@@ -187,7 +162,7 @@ Partial Class frmTransaksi
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(480, 155)
+        Me.Label4.Location = New System.Drawing.Point(480, 144)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(40, 22)
         Me.Label4.TabIndex = 15
@@ -197,7 +172,7 @@ Partial Class frmTransaksi
         '
         Me.txtTunai.AutoSize = True
         Me.txtTunai.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtTunai.Location = New System.Drawing.Point(38, 320)
+        Me.txtTunai.Location = New System.Drawing.Point(38, 297)
         Me.txtTunai.Name = "txtTunai"
         Me.txtTunai.Size = New System.Drawing.Size(56, 22)
         Me.txtTunai.TabIndex = 16
@@ -207,7 +182,7 @@ Partial Class frmTransaksi
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(458, 327)
+        Me.Label6.Location = New System.Drawing.Point(458, 304)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(98, 22)
         Me.Label6.TabIndex = 17
@@ -215,24 +190,24 @@ Partial Class frmTransaksi
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(567, 330)
+        Me.TextBox4.Location = New System.Drawing.Point(567, 306)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(160, 20)
         Me.TextBox4.TabIndex = 18
         '
         'TextBox5
         '
-        Me.TextBox5.Location = New System.Drawing.Point(104, 323)
+        Me.TextBox5.Location = New System.Drawing.Point(104, 300)
         Me.TextBox5.Name = "TextBox5"
         Me.TextBox5.Size = New System.Drawing.Size(151, 20)
         Me.TextBox5.TabIndex = 19
         '
         'frmTransaksi
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Beige
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 418)
         Me.Controls.Add(Me.TextBox5)
         Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.Label6)
@@ -261,9 +236,6 @@ Partial Class frmTransaksi
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents btnBayar As Button
     Friend WithEvents btnBack As Button
     Friend WithEvents Label1 As Label
@@ -273,7 +245,6 @@ Partial Class frmTransaksi
     Friend WithEvents Kasir As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents Column4 As DataGridViewTextBoxColumn
     Friend WithEvents NumericUpDown1 As NumericUpDown
     Friend WithEvents Label3 As Label
     Friend WithEvents TextBox1 As TextBox
--- PI-Ardhy/frmTransaksi.vb
+++ PI-Ardhy/frmTransaksi.vb
@@ -1,4 +1,5 @@
 ﻿Imports MySql.Data.MySqlClient
+Imports System.Globalization
 
 Public Class frmTransaksi
     Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
@@ -15,21 +16,85 @@ Public Class frmTransaksi
         Dim test = ds.Tables(0).Rows.Count
         For i As Integer = 0 To ds.Tables(0).Rows.Count - 1
 
-            ComboBox1.Items.Add(ds.Tables(0).Rows(i).Item("nama_barang"))
+            Dim a = ds.Tables(0).Rows(i).Item("nama_barang")
+            Dim b = ds.Tables(0).Rows(i).Item("kd_barang")
+            ComboBox1.DataSource = ds.Tables(0)
+            ComboBox1.ValueMember = "kd_barang"
+            ComboBox1.DisplayMember = "nama_barang"
 
         Next
 
     End Sub
 
-    Private Sub DomainUpDown1_SelectedItemChanged(sender As Object, e As EventArgs)
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim test = ComboBox1.SelectedValue
+        Dim qty As Integer = NumericUpDown1.Value
+
+        Dim table As New DataTable()
+        Dim adapter As New MySqlDataAdapter($"SELECT kd_barang as 'Kode Barang', nama_barang as 'Nama Barang', harga_jual as Harga, {qty} as qty, (harga_jual * {qty}) as Total FROM stock where kd_barang = {test}", strConn)
+
+        If DataGridView1.DataSource IsNot Nothing Then
+            Dim table2 As New DataTable()
+            adapter.Fill(table2)
+            Dim total = 0
+            Dim table3 As DataTable = DataGridView1.DataSource
+
+            ' Loop through each row in the source DataTable
+            For Each sourceRow As DataRow In table2.Rows
+                ' Import the row from the source DataTable to the destination DataTable
+                Dim newRow As DataRow = table3.NewRow()
+
+                ' Copy the values from sourceRow to newRow
+                For Each column As DataColumn In table3.Columns
+                    newRow(column.ColumnName) = sourceRow(column.ColumnName)
+                Next
+
+                ' Add the new row to the destination DataTable
+                table3.Rows.Add(newRow)
+            Next
+
+            DataGridView1.DataSource = table3
+
+            For i As Integer = 0 To table3.Rows.Count - 1
+
+                Dim totalrow As Integer = table3.Rows(i).Item("Total")
+
+                total += totalrow
+
+            Next
+
+            Dim formattedValue As String = String.Format("{0:C2}", total)
+            TextBox1.Text = formattedValue
+
+            MsgBox("sukses tambah kue...")
+        Else
+            adapter.Fill(table)
+
+            DataGridView1.DataSource = table
+
+            Dim total = table.Rows(0).Item("Total")
+            Dim formattedValue As String = String.Format("{0:C2}", total)
+
+            TextBox1.Text = formattedValue
+
+            MsgBox("sukses tambah kue...")
+        End If
 
     End Sub
 
-    Private Sub txtDate_TextChanged(sender As Object, e As EventArgs) Handles txtDate.TextChanged
+    Private Sub btnBayar_Click(sender As Object, e As EventArgs) Handles btnBayar.Click
+        Dim bayar = TextBox5.Text
+        Dim total = TextBox1.Text
 
-    End Sub
+        Dim kembalian = bayar - total
 
-    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles txtTunai.Click
+        Dim formattedValue As String = String.Format("{0:C2}", kembalian)
+
+        TextBox4.Text = formattedValue
+
+
+
+
 
     End Sub
 End Class
